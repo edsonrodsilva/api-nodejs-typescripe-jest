5212ffe6f0159ee7c05e286f595832fe02e8ca97
Edson Rodrigues <edsonrodsilva@gmail.com>
2022-06-08 11:27:11 -0300

    first commit

---
+++ test/functional/forecast.test.ts
@@ -0,0 +1,8 @@
+import supertest from "supertest";
+describe('Beach forecast function test', () => {
+    it('should return a forecast with just a few times', async() => {
+      const {body, status} = await supertest(app).get('/forecast');
+      expect(status).toBe(200);
+      expect(body);
+    })
+})
